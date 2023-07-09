f36a9dc933cf75a1b837938e834562ba71e5807a
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-07-09 16:24:59 +0700

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-cd ~/express-example && npm i && pm2 restart api
+# using pm2
+# cd ~/express-example && npm i && pm2 restart api
+
+# using docker local
+DOCKER_IMAGE="high10hunter/node"
+
+# Stop and remove the container
+docker kill express-example
+docker rm express-example
+docker rmi $DOCKER_IMAGE
+
+# Build the Docker image
+docker build -t $DOCKER_IMAGE .
+docker run -p 3000:3000 --name express-example -d $DOCKER_IMAGE
